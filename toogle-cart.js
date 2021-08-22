f0fa30835184f8bec27e6365bcd29f0c42ef4271
freshervn <thefagogroup>
2021-08-22 20:10:29 +0700

    done header

--- toogle-cart.js
+++ toogle-cart.js
@@ -1,16 +1,44 @@
 $(document).ready(
     function(){
-        $('.toggle-cart').click(
-            function(){
-                $('.OTC-cart-sidebar').toggleClass('active');
-                console.log('run');
+        // $('.toggle-cart').click(
+        //     function(){
+        //         $('.OTC-cart-sidebar').toggleClass('active');
+        //         console.log('run');
+        //     }
+        // )    
+        // $('.toogle-menu-mobie').click(
+        //     function(){
+        //         $(' header nav .links').toggleClass('mobie-on');
+        //         console.log('run');
+        //     }
+        // )    
+        // khi một nút click dc bấm
+        $('.click-show').click(
+            function () {
+                // tìm khối cần hiển thị
+                // kiểm tra tình trạng hiện, ẩn
+                $blockId = $('.' + $(this).attr('data-block-id'));
+                $blockId.toggleClass('mobie-on');
             }
-        )    
-        $('.toogle-menu-mobie').click(
-            function(){
-                $(' header nav .links').toggleClass('mobie-on');
-                console.log('run');
+        )
+        // khi di chuot vào khối thì tắt uwwindown listentd
+        $('.ClickToShow,button,.click-show').mouseover(
+            function () {
+                // console.log('chuot hover button');
+                $(window).off('mousedown');
             }
-        )    
+        )
+        $('.ClickToShow , button,.click-show').mouseleave(
+            function () {
+                // console.log('chuot ben ngoai button');
+                // khi di chuột    
+                $(window).mousedown(
+                    function () {
+                        $blockId = $('.' + $('.click-show').attr('data-block-id'));
+                        $blockId.toggleClass('mobie-on');
+                    }
+                );
+            }
+        )
     }
 )
