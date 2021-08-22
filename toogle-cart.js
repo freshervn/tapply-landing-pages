90ec0730c6528b9350fb49c1361827b5f6821b27
freshervn <thefagogroup>
2021-08-22 22:24:07 +0700

    fix

--- toogle-cart.js
+++ toogle-cart.js
@@ -1,11 +1,11 @@
 $(document).ready(
     function(){
-        // $('.toggle-cart').click(
-        //     function(){
-        //         $('.OTC-cart-sidebar').toggleClass('active');
-        //         console.log('run');
-        //     }
-        // )    
+        $('.toggle-cart').click(
+            function(){
+                $('.OTC-cart-sidebar').toggleClass('active');
+                // console.log('run');
+            }
+        )    
         // $('.toogle-menu-mobie').click(
         //     function(){
         //         $(' header nav .links').toggleClass('mobie-on');
