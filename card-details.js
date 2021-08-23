d9a3276f37a656ac4405b271ab72e4971579a347
freshervn <thefagogroup>
2021-08-23 20:24:40 +0700

    let check

--- card-details.js
+++ card-details.js
@@ -22,3 +22,36 @@ $('.small-to-big-window .small-item').click(
         var item = $(this).clone();
         $('.small-to-big-window  .big-window ').html(item);
 });
+$('.select-color').click(function(){
+    $color=$(this).find('.checkmark').css('color');
+    $('.small .small-item.main-card')[0].click();
+    $('.main-card .card-color').css('fill',$color);
+    if (wc_hex_is_light(rgbToHex($color))) {
+        $('.main-card .invent-color').css('fill','black');
+        $('.main-card span').css('color','black'); 
+        $('.main-card .invent-img').css('filter','invert(1)');   
+    }
+    else{
+        $('.main-card .invent-color').css('fill','white');
+        $('.main-card .invent-img').css('filter','invert(0)');          
+        $('.main-card span').css('color','white');  
+    }
+})
+function rgbToHex(c){
+    var rgb =c ;    
+    rgb = rgb.substring(4, rgb.length-1)
+             .replace(/ /g, '')
+             .split(',');
+    return rgb;
+}
+function wc_hex_is_light([r,g,b]) {
+    const c_r = parseInt(r);
+    const c_g = parseInt(g);
+    const c_b = parseInt(b);
+    const brightness = ((c_r * 299) + (c_g * 587) + (c_b * 114)) / 1000;
+    return brightness > 155;
+}
+$('input[name="tapply-name"]').keydown(function(){
+    $('.small .small-item.main-card')[0].click();
+    $('.main-card span').text($(this).val());
+})
